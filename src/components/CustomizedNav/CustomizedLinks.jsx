3c855e542cfde85eef390e6800445ffc55a6f01b
afsana43 <afsanahamidmim70@gmail.com>
2024-12-21 20:43:24 +0600

    navbar done

---
+++ src/components/CustomizedNav/CustomizedLinks.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+
+const CustomizedLinks = ({route}) => {
+  return (
+    <li className="mr-6 hover:bg-yellow-100 px-6 py-3 rounded-sm" > 
+    <a href={route.path}>{route.name}</a>
+    </li>)
+ 
+};
+ 
+
+// PropTypes added..............
+
+CustomizedLinks.propTypes ={
+  route:PropTypes.object
+}
+export default CustomizedLinks;
---
+++ src/components/CustomizedNavBar/CustomizedNavBar.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import CustomizedLinks from "../CustomizedNav/CustomizedLinks";
+import { IoMenuOutline } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+
+const CustomizedNavBar = () => {
+
+const [open, setOpen] = useState(false);
+
+  const routes = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/home", },
+      {
+      id: 2,
+      name: "About",
+      path: "/about",
+    },
+    {
+      id: 3,
+      name: "Products",
+      path: "/products",
+    },
+    {
+      id: 4,
+      name: "Contact",
+      path: "/contact",
+    },
+    {
+      id: 5,
+      name: "FAQ",
+      path: "/faq",
+    },
+  ];
+
+
+  return <nav>
+    <div className="text-3xl md:hidden" onClick={()=> setOpen(!open)}>
+      {
+        open === true ? <IoMdClose /> : < IoMenuOutline/> 
+      }
+    
+    </div>
+
+   <ul className={`md:flex absolute duration-100 ${open ? '' : 'hidden'}`}> 
+   {
+        routes.map(route=> <CustomizedLinks key={route.id} route={route}></CustomizedLinks>)
+    }
+   </ul>
+  </nav>;
+};
+
+export default CustomizedNavBar;
+
+
+
+// toggole korar jnno 3ta kaj krte hoi..............
+/**
+ * 1. use state
+ * 2.use onclick handle for btn
+ * 3.use 
+ */
